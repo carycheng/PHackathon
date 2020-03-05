68a92dcff183d89f69801c7ee8723e74a76ce044
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-03-05 14:17:09 -0800

    wip

--- index.js
+++ index.js
@@ -14,48 +14,23 @@ let sdk = new BoxSDK({
 	iterators: true
 });
 
-var token = process.env.token;
 var client = sdk.getBasicClient(token);
 
-exports.generateUser =(req, res) => {
-	client.folders.getItems('0',
+exports.generateUser = async (req, res) => {
+var folder = client.folders.getItems('0',
 	{
 		usemarker: false,
 		limit: 10
-	}).then(items => {
-		console.log(items)
-		res.status(200).send(items)
 	});
-}
+res.status(200).send(items)
 
 // var login = process.env.login;
 // var name = process.env.name;
 
-// client.enterprise.addUser(
+// var user = await client.enterprise.addUser(
 // 	login,
 // 	name,
 // 	{
-// 		role: client.enterprise.userRoles.CO,
-// 		status: client.enterprise.userStatuses.CANNOT_DELETE_OR_EDIT
-// 	})
-// 	.then(user => {
-// 		/* user -> {
-// 			type: 'user',
-// 			id: '44444',
-// 			name: 'Ned Stark',
-// 			login: 'eddard@winterfell.example.com',
-// 			created_at: '2012-11-15T16:34:28-08:00',
-// 			modified_at: '2012-11-15T16:34:29-08:00',
-// 			role: 'coadmin',
-// 			language: 'en',
-// 			timezone: 'America/Los_Angeles',
-// 			space_amount: 5368709120,
-// 			space_used: 0,
-// 			max_upload_size: 2147483648,
-// 			status: 'active',
-// 			job_title: '',
-// 			phone: '',
-// 			address: '555 Box Lane',
-// 			avatar_url: 'https://www.box.com/api/avatar/large/deprecated' }
-//         */
+// 		role: client.enterprise.userRoles.USER
 // 	});
+}
